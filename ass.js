b10b1825ad77cbd674f5df2f96dd592ca8475136
preciousgift2211 <digitallydiamond@gmail.com>
2026-08-02 22:22:40 +0100

    Create ass.js

---
+++ ass.js
@@ -0,0 +1,43 @@
+let num = -2;
+if (num > 0) {
+    console.log("the number is positive ");
+    
+}else if (num < 0) {
+    console.log("the number is negative");
+
+}else {
+console.log("the number is zero");
+
+}
+
+let age = 14;
+if (age >= 15 ) {
+    console.log("u're eligible to register");
+    
+} else{
+console.log("u're not eligible to register");
+
+}
+let scur = 70
+if (scur >= 70) {
+    console.log("grade A");
+    
+} else if (scur >= 60) {
+    console.log("grade B");
+    
+} else if (scur >= 50 ) {
+    console.log("grade C");
+    
+} else if (scur >= 40) {
+    console.log("grade D");
+    
+} else if (scur >= 30 ){
+    console.log("grade E");
+    
+}else {
+    console.log("grade F");
+    
+}
+
+
+
